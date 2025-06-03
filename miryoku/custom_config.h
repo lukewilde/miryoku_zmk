53c79a52cd704415008fc769451db3eb2de96e79
wilde <luke.wilde@gmail.com>
2025-06-03 21:28:34 +0100

    Mouse keys and game layers

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,7 +2,7 @@
 // https://github.com/manna-harbour/miryoku
 
 #define MIRYOKU_NAV_INVERTEDT
-#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+// #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 // #define XXX &none
 
 #define MIRYOKU_LAYER_BASE \
@@ -12,7 +12,7 @@ U_MT(LCTRL, Z),    U_MT(RALT, X),     &kp C,             &kp D,             &kp 
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
-&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp LBRC,          &kp AMPS,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       &studio_unlock,            \
 &kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp PIPE2,        &kp EXCL,          &kp AT,            &kp HASH,          &kp TILDE2,         U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
@@ -29,52 +29,53 @@ U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp 
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp K_APP,         &kp SPACE,         &kp GRAVE,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-// #define MIRYOKU_LAYER_GAME \
-// &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,              &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         \
-// &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
-// &kp Z,             &kp X,             &kp C,             &kp V,             &kp B,             &kp N,              &kp LBKT,          &kp COMMA,         &kp DOT,           &kp RBKT,         \
-// U_NP,              U_NP,              &kp ESC,           &kp SPC,           &kp LCTRL,         U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+#define MIRYOKU_LAYER_GAME_1 \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp NON_US_BACKSLASH,   &kp N1,            &kp N2,            &kp N3,            &kp NON_US_HASH,       U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-// #define MIRYOKU_LAYER_GAME_2 \
-//   &kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,              &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp NUM_4,         \
-//   &kp A,             &kp S,             &kp D,             &kp F,             &kp G,             &kp H,              U_MS_L,            U_MS_D,            U_MS_U,            U_MS_R,            \
-// &kp NON_US_BACKSLASH,&kp Z,             &kp X,             &kp C,             &kp V,             &kp N,              &kp LBKT,          &kp COMMA,         &kp DOT,           &kp RBKT,         \
-//   U_NP,              U_NP,              &kp ESC,           &kp SPC,           &kp LCTRL,         U_BTN2,            U_BTN1,            U_BTN3,            U_NP,              U_NP
+#define MIRYOKU_LAYER_GAME_2 \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp NON_US_BACKSLASH,   &kp N1,            &kp N2,            &kp N3,            &kp NON_US_HASH,       U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-// #define MIRYOKU_LAYOUTMAPPING_CORNE( \
-//      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
-//      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
-//      K20, K21, K22, K23, K24,      K25, K26, K27, K28, K29, \
-//      N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
-// ) \
-// &kp GRAVE  K00  K01  K02  K03  K04       K05  K06  K07  K08  K09  &to U_GAME \
-// &kp TAB  K10  K11  K12  K13  K14       K15  K16  K17  K18  K19  XXX \
-// &kp LSHFT  K20  K21  K22  K23  K24       K25  K26  K27  K28  K29  &to U_BASE \
-//                K32  K33  K34       K35  K36  K37
+#define MIRYOKU_LAYER_GAME_3 \
+&kp LBKT,          &kp N7,            &kp N8,            &kp N9,            &kp RBKT,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp SEMI,          &kp N4,            &kp N5,            &kp N6,            &kp EQUAL,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp NON_US_BACKSLASH,   &kp N1,            &kp N2,            &kp N3,            &kp NON_US_HASH,       U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp DOT,           &kp N0,            &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-// #define MIRYOKU_LAYER_LIST \
-// MIRYOKU_X(BASE,   "Base") \
-// MIRYOKU_X(EXTRA,  "Extra") \
-// MIRYOKU_X(TAP,    "Tap") \
-// MIRYOKU_X(BUTTON, "Button") \
-// MIRYOKU_X(NAV,    "Nav") \
-// MIRYOKU_X(MOUSE,  "Mouse") \
-// MIRYOKU_X(MEDIA,  "Media") \
-// MIRYOKU_X(NUM,    "Num") \
-// MIRYOKU_X(SYM,    "Sym") \
-// MIRYOKU_X(FUN,    "Fun") \
-// MIRYOKU_X(GAME,   "Gaming")
+#define MIRYOKU_LAYER_LIST \
+MIRYOKU_X(BASE,   "Base") \
+MIRYOKU_X(EXTRA,  "Extra") \
+MIRYOKU_X(TAP,    "Tap") \
+MIRYOKU_X(BUTTON, "Button") \
+MIRYOKU_X(NAV,    "Nav") \
+MIRYOKU_X(MOUSE,  "Mouse") \
+MIRYOKU_X(MEDIA,  "Media") \
+MIRYOKU_X(NUM,    "Num") \
+MIRYOKU_X(SYM,    "Sym") \
+MIRYOKU_X(FUN,    "Fun") \
+MIRYOKU_X(GAME_1,    "GAME_1") \
+MIRYOKU_X(GAME_2,   "GAME_2") \
+MIRYOKU_X(GAME_3,   "GAME_3")
 
-// #define MIRYOKU_LAYERMAPPING_GAME MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_GAME_1 MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_GAME_2 MIRYOKU_MAPPING
+#define MIRYOKU_LAYERMAPPING_GAME_3 MIRYOKU_MAPPING
 
-// #define U_BASE   0
-// #define U_EXTRA  1
-// #define U_TAP    2
-// #define U_BUTTON 3
-// #define U_NAV    4
-// #define U_MOUSE  5
-// #define U_MEDIA  6
-// #define U_NUM    7
-// #define U_SYM    8
-// #define U_FUN    9
-// #define U_GAME   10
+#define U_BASE   0
+#define U_EXTRA  1
+#define U_TAP    2
+#define U_BUTTON 3
+#define U_NAV    4
+#define U_MOUSE  5
+#define U_MEDIA  6
+#define U_NUM    7
+#define U_SYM    8
+#define U_FUN    9
+#define U_GAME_1   10
+#define U_GAME_2   11
+#define U_GAME_3   12
